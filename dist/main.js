a4508f2b96c7fda71c8acd224051928035b2f3e9
José Antônio (Working from Home) <jgomes@brasilcard.net>
2021-02-28 12:28:44 -0300

    added configuration to app, trough environment variables and config yml files

--- dist/main.js
+++ dist/main.js
@@ -3,10 +3,12 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const core_1 = require("@nestjs/core");
 const common_1 = require("@nestjs/common");
 const app_module_1 = require("./app.module");
+const config = require("config");
 async function bootstrap() {
+    const serverConfig = config.get('server');
     const logger = new common_1.Logger('boostrap');
     const app = await core_1.NestFactory.create(app_module_1.AppModule);
-    const port = 3000;
+    const port = process.env.PORT || serverConfig.port;
     await app.listen(port);
     logger.log(`Application listening on port ${port}`);
 }
